3500e8a8d895dbd090d04200835f5ac7b204fd8b
Waylon <wayloncn@gmail.com>
2021-03-23 12:14:08 -0400

    setup and first fetch to strapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'strapi-angular-frontend';
+  restaurants: any[] = [];
+  error = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('http://localhost:1337/restaurants')
+      .subscribe((response: any) => {
+        this.restaurants = response;
+      });
+  }
 }
