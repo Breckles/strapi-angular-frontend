de99d6c281bc7da1831de2d6cdc910dbb2520a45
Waylon <wayloncn@gmail.com>
2021-03-23 14:22:58 -0400

    created a form and implemented a push operation to strapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +14,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'strapi-angular-frontend';
   restaurants: any[] = [];
+  allCategories: any[] = [];
   error = null;
+  restaurantForm!: FormGroup;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private fbService: FormBuilder) {}
 
   ngOnInit() {
     this.http
@@ -19,5 +27,51 @@ export class AppComponent implements OnInit {
       .subscribe((response: any) => {
         this.restaurants = response;
       });
+
+    this.http
+      .get('http://localhost:1337/categories')
+      .subscribe((response: any) => {
+        this.allCategories = response;
+      });
+
+    this.restaurantForm = this.fbService.group({
+      name: '',
+      description: '',
+      categories: new FormArray([]),
+    });
+  }
+
+  onCheckChange(event: Event) {
+    console.log(event);
+    const checkbox = event.target as HTMLInputElement;
+
+    const formArray: FormArray = this.restaurantForm.get(
+      'categories'
+    ) as FormArray;
+
+    if (checkbox.checked) {
+      formArray.push(new FormControl(checkbox.value));
+    } else {
+      let i: number = 0;
+
+      formArray.controls.forEach((ctrl: AbstractControl) => {
+        if (ctrl.value == checkbox.value) {
+          formArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+
+  onSubmit() {
+    this.http
+      .post('http://localhost:1337/restaurants', this.restaurantForm.value)
+      .subscribe((response) => {
+        console.log(response);
+        // console.log(this.restaurantForm.value);
+      });
+
+    this.restaurantForm.reset();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +8,12 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
